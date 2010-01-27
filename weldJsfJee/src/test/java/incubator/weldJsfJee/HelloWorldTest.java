4db6898b10fb8fd4ab7cc26165556b1edbf44225
michaelschuetz83 <michaelschuetz83@cc77e4ac-0099-11df-b136-7f7962f7bc17>
2010-01-27 10:53:03 +0000

    crud-sample completed refactoring, repackaging and implementing core CDI features

---
+++ weldJsfJee/src/test/java/incubator/weldJsfJee/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package incubator.weldJsfJee;
+
+import org.testng.annotations.Test;
+
+/**
+ * Dummmy Test
+ * 
+ * @author schuetz
+ * @date 11.01.2010
+ */
+public class HelloWorldTest {
+	@Test
+	public void testGetText() {
+		// TODO implement real tests
+		// HelloWorld fixture = new HelloWorld();
+		// assert "Hello World!".equals(fixture.getText());
+	}
+}
