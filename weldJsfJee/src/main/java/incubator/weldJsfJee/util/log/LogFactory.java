4193e2675f0716ca86b36a30af224913c378322b
michaelschuetz83 <michaelschuetz83@cc77e4ac-0099-11df-b136-7f7962f7bc17>
2010-01-27 21:56:06 +0000

    added weld-logger dependency

--- weldJsfJee/src/main/java/incubator/weldJsfJee/util/log/LogFactory.java
+++ weldJsfJee/src/main/java/incubator/weldJsfJee/util/log/LogFactory.java
@@ -5,8 +5,14 @@ import java.util.logging.Logger;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 
+import org.jboss.weld.log.LoggerProducer;
+
 /**
  * Wraps Logger-Implementation. Defines InjectionPoint for use with @Inject.
+ * 
+ * TODO Delete this file after bug is fixed with LoggerProducer.
+ * 
+ * @see LoggerProducer
  * 
  * @author schuetz
  * @date 11.01.2010
