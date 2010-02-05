bd324e724db35e794d0bf14e3ecdc96a4b5d187b
michaelschuetz83 <michaelschuetz83@cc77e4ac-0099-11df-b136-7f7962f7bc17>
2010-02-05 00:43:59 +0000

    initial

--- trunk/weldGae/src/incubator/weldGae/HelloWorld.java
+++ trunk/weldGae/src/incubator/weldGae/HelloWorld.java
@@ -3,15 +3,14 @@ package incubator.weldGae;
 import javax.inject.Named;
 
 /**
+ * Simple dummy CDI-Bean.
  * 
  * @author m.schuetz
- *
+ * @date 04.02.2010
  */
 @Named
-public class HelloWorld
-{
-   public String getMessage()
-   {
-      return "Hello, World 2010";
-   }
+public class HelloWorld {
+	public String getMessage() {
+		return "Hello Weld World";
+	}
 }
