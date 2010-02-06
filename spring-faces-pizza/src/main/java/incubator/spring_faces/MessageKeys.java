70fd424b35c98f4ec75ead277c6fff7994b4aad2
marek.amos <marek.amos@cc77e4ac-0099-11df-b136-7f7962f7bc17>
2010-02-06 21:41:53 +0000

    initial commit

---
+++ spring-faces-pizza/src/main/java/incubator/spring_faces/MessageKeys.java
@@ -0,0 +1,27 @@
+package incubator.spring_faces;
+
+/**
+ * Provides keys for error messages and labels.
+ * 
+ * @author iwaszkiewicz
+ * @date 06.02.2010
+ */
+public interface MessageKeys {
+
+    /*
+     * ----- error -----
+     */
+
+    public static final String ERROR_PHONENUMBER = "error.input.phonenumber";
+
+    public static final String ERROR_USER_PERSIST_FAILED = "error.confirmCustomer.persist";
+
+    public static final String ERROR_ORDER_PERSIST_FAILED = "error.confirmOrder.persist";
+
+    public static final String ERROR_UNEXPECTED = "error.unexpected";
+
+    /*
+     * ----- label -----
+     */
+
+}
