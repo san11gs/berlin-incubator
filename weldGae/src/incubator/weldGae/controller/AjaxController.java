e2a1238a8fabdebd7b636d5ba09805f9d2c8b337
michaelschuetz83 <michaelschuetz83@cc77e4ac-0099-11df-b136-7f7962f7bc17>
2010-02-24 22:50:00 +0000

    added example for native jsf 2.0 ajax support

---
+++ weldGae/src/incubator/weldGae/controller/AjaxController.java
@@ -0,0 +1,24 @@
+package incubator.weldGae.controller;
+
+import java.io.Serializable;
+
+/**
+ * Ajax-Controller. Ajax stuff is triggered directly from the JSF GUI.
+ * 
+ * @author schuetz
+ * @date 10.01.2010
+ */
+@GuiController
+public class AjaxController implements Serializable {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
