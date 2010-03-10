a860d33e6b68780371b09cfd7c98c7176d0aacd7
michaelschuetz83 <michaelschuetz83@cc77e4ac-0099-11df-b136-7f7962f7bc17>
2010-03-10 02:11:32 +0000

    gae ant target, highlight cause for JPA problems

--- spring-flex-gae/server/src/main/java/incubator/spring_flex/service/CustomerServiceImpl.java
+++ spring-flex-gae/server/src/main/java/incubator/spring_flex/service/CustomerServiceImpl.java
@@ -1,13 +1,15 @@
 package incubator.spring_flex.service;
 
+import javax.persistence.EntityManager;
+
 import incubator.spring_flex.domain.Customer;
+import incubator.spring_flex.persistence.GAE_EntityManagerFactory;
 import incubator.spring_flex.repository.CustomerRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.flex.remoting.RemotingDestination;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
-
 
 /**
  * Service for loading and saving customers (business delegate pattern!!).
@@ -21,40 +23,54 @@ public class CustomerServiceImpl implements CustomerService {
 
 	@Autowired
 	private CustomerRepository customerRepository;
-	
-    public boolean existsCustomer(String phoneNumber) {
-        return this.customerRepository.existsCustomer(phoneNumber);
-    }
 
-    /**
-     * Transactional read-only method.
-     * 
-     * @see incubator.spring_flex.service.CustomerService#findCustomerByPhoneNumber(String)
-     */
-    public Customer findCustomerByPhoneNumber(String phoneNumber) {
-    	return this.customerRepository.findCustomerByPhoneNumber(phoneNumber);
-    }
+	public boolean existsCustomer(String phoneNumber) {
+		return this.customerRepository.existsCustomer(phoneNumber);
+	}
 
-    /**
-     * Transactional method.
-     * 
-     * @see incubator.spring_flex.service.CustomerService#createCustomer(Customer)
-     */
-    @Secured( {"ROLE_USER", "ROLE_SUPERVISOR"} )
-    public Customer createCustomer(Customer customer) {    	
-        return this.customerRepository.createCustomer(customer);
-    }
+	/**
+	 * Transactional read-only method.
+	 * 
+	 * @see incubator.spring_flex.service.CustomerService#findCustomerByPhoneNumber(String)
+	 */
+	public Customer findCustomerByPhoneNumber(String phoneNumber) {
+		return this.customerRepository.findCustomerByPhoneNumber(phoneNumber);
+	}
 
-    /*
-     * Getter & Setter
-     */
-    
-    public CustomerRepository getCustomerManager() {
-    	return this.customerRepository;
-    }
-    
-    public void setCustomerManager(CustomerRepository customerManager) {
-    	this.customerRepository = customerManager;
-    }
-    
+	/**
+	 * Transactional method.
+	 * 
+	 * @see incubator.spring_flex.service.CustomerService#createCustomer(Customer)
+	 */
+	@Secured( { "ROLE_USER", "ROLE_SUPERVISOR" })
+	public Customer createCustomer(Customer customer) {
+		// return this.customerRepository.createCustomer(customer);
+
+		// TODO (m.schuetz ) --> core problem is ID --> use KEY --> Problem
+		// with SWC!
+		// additionally, spring jta is not working with GAE out of the box)
+		GAE_EntityManagerFactory emf = new GAE_EntityManagerFactory("pizza-pu");
+		EntityManager em = emf.entityManagerFactory().createEntityManager();
+
+		try {
+			em.persist(customer);
+		} finally {
+			em.close();
+		}
+
+		return customer;
+	}
+
+	/*
+	 * Getter & Setter
+	 */
+
+	public CustomerRepository getCustomerManager() {
+		return this.customerRepository;
+	}
+
+	public void setCustomerManager(CustomerRepository customerManager) {
+		this.customerRepository = customerManager;
+	}
+
 }
